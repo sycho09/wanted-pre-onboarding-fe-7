ab87c09a4e83e3b15fcce9c9a10e4b7163d6e489
sycho09 <sycho09@users.noreply.github.com>
2022-10-11 22:23:34 +0000

    deploy: 876f4dc4046259feaa1f24797069e91af8d7586d

--- src/utils/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-import { BASE_URL } from "../utils/constants";
-
-export const TODO_API = axios.create({
-  baseURL: BASE_URL,
-});
--- src/utils/constants.js
+++
@@ -1,3 +0,0 @@
-export const REG_EXP =
-  /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
-export const BASE_URL = "https://pre-onboarding-selection-task.shop";
